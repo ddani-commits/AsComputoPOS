490546e8e2f861f7bc5d2b67e3128cae449febf2
ddani-commits <23090584@alumno.utmetropolitana.edu.mx>
2025-05-15 11:39:31 -0600

    retrieving data from database usin entity framework core

---
+++ AsComputoPOS/ViewModels/EmployeesViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using AsComputoPOS.Services;
+using AsComputoPOS.Models;
+using AsComputoPOS.Data;
+
+namespace AsComputoPOS.ViewModels
+{
+    public partial class EmployeesViewModel: NavigationBarViewModel
+    {   
+        public ObservableCollection<Employee> EmployeesList { get;  } = new();
+        public EmployeesViewModel(INavigationService navigation) : base(navigation)
+        {
+            LoadEmployees();
+        }
+
+        private void LoadEmployees()
+        {
+            using var db = new ApplicationDbContext();
+            foreach(var employee in db.Employees)
+            {
+                EmployeesList.Add(employee);
+            }
+        }
+    }
+}
