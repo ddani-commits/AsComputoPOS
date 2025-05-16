f44801448a37327b59ff92842e5f06f56e75c2bc
LiliAbi-ne <contrerass.cristina@gmail.com>
2025-05-16 13:58:46 -0600

    Supplier Model

---
+++ AsComputoPOS/Models/Supplier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsComputoPOS.Models
+{
+    public class Supplier
+    {
+        public int SupplierId { get; set; }
+        public string Name { get; set; }
+        public string ContactName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool isActive { get; set; }
+
+        public Supplier(string name, string contactName, string address, string email, string phone, bool isActive)
+        {
+            Name = name;
+            ContactName = contactName;
+            Address = address;
+            Email = email;
+            Phone = phone;
+            this.isActive = isActive;
+        }
+
+
+    }
+}
