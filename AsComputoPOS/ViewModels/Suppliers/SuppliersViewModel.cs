dce1e084c1473ba207d726c80158bc4a5e18914c
LiliAbi-ne <contrerass.cristina@gmail.com>
2025-05-20 09:22:55 -0600

    Suppliers View and VM

--- AsComputoPOS/ViewModels/Suppliers/SuppliersViewModel.cs
+++ AsComputoPOS/ViewModels/Suppliers/SuppliersViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using AsComputoPOS.Data;
 using AsComputoPOS.Services;
+using CommunityToolkit.Mvvm.Input;
 namespace AsComputoPOS.ViewModels.Suppliers
 {
     public partial class SuppliersViewModel : NavigationBarViewModel
@@ -23,5 +26,62 @@ namespace AsComputoPOS.ViewModels.Suppliers
                 SuppliersList.Add(supplier);
             }
         }
+
+        public string Name { get; set; } = "";
+        public string ContactName { get; set; } = "";
+        public string Address { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public bool IsActive { get; set; } = false;
+
+        // Comando para añadir un proveedor
+        public void AddSupplier(string name, string contactName, string address, string email, string phone, bool isActive)
+        {
+            using var db = new ApplicationDbContext();
+            var supplier = new Models.Supplier(name, contactName, address, email, phone, isActive);
+            db.Suppliers.Add(supplier);
+            db.SaveChanges();
+            SuppliersList.Add(supplier);
+        }
+
+        [RelayCommand]
+        public void SaveSupplier()
+        {
+            if (string.IsNullOrWhiteSpace(Name) || string.IsNullOrWhiteSpace(ContactName) || string.IsNullOrWhiteSpace(Address) || string.IsNullOrWhiteSpace(Email) || string.IsNullOrWhiteSpace(Phone))
+            {
+                Debug.WriteLine("Please fill in all fields.");
+                return;
+            }
+            AddSupplier(Name, ContactName, Address, Email, Phone, IsActive);
+            Name = "";
+            ContactName = "";
+            Address = "";
+            Email = "";
+            Phone = "";
+            IsActive = true;
+            Debug.WriteLine("Supplier added successfully.");
+
+        }
+
+        // Comando para eliminar un proveedor
+        
+
+        [RelayCommand]
+        public void DeleteSupplier(Models.Supplier supplier)
+        {
+            if (supplier == null)
+            {
+                Debug.WriteLine("Supplier not found.");
+                return;
+            }
+            using var db = new ApplicationDbContext();
+            db.Suppliers.Attach(supplier);
+            db.Suppliers.Remove(supplier);
+            db.SaveChanges();
+            SuppliersList.Remove(supplier);
+        }
+
+
+
     }
 }
