0e12218add4cec64983da2a1ecb465cdee22afac
ddani-commits <23090584@alumno.utmetropolitana.edu.mx>
2025-06-17 12:19:31 -0600

    add productId property

--- AsComputoPOS/Models/PurchaseOrder.cs
+++ AsComputoPOS/Models/PurchaseOrder.cs
@@ -1,23 +1,16 @@
-﻿using TamoPOS.Models;
-
-namespace TamoPOS.Models
+﻿namespace TamoPOS.Models
 {
     // Represents the general term of purchase order, which is a request to buy products from a supplier
     public class PurchaseOrder 
     {
         public int Id { get; set; }
-        
         public int SupplierId { get; set; }
         public Supplier Supplier { get; set; }
-        
         public DateTime PurchaseDate { get; set; }
         public decimal? Total {get; set;}
         public decimal? Subtotal {get; set;}
-
         public ICollection<ProductPurchase>? ProductPurchases { get; set; }
-
         public PurchaseOrder() { }
-
         public PurchaseOrder(Supplier supplier, DateTime purchaseDate) 
         {
             Supplier = supplier;
@@ -28,6 +21,7 @@ namespace TamoPOS.Models
     // Product purchase represents each one of the items that are being purchased in a purchase order
     public class ProductPurchase {
         public int Id { get; set; }
+        public int ProductId { get; set; }
         public Product Product {get; set;} // references the product being resupplied
         public decimal UnitPrice {get; set;} // represents the purchase price from the business perspective
         public decimal Quantity {get; set;} // represents how many units were bougth in this batch
