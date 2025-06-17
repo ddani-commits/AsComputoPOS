480241a5195d7237d1558fd74b56cfb2ded2c351
ddani-commits <23090584@alumno.utmetropolitana.edu.mx>
2025-06-17 13:59:43 -0600

    create product purchase conted dialog

---
+++ AsComputoPOS/Controls/NewProductPurchaseContentDialog.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+using TamoPOS.Models;
+using Wpf.Ui.Controls;
+
+namespace TamoPOS.Controls
+{
+    /// <summary>
+    /// Lógica de interacción para NewProductPurchaseContentDialog.xaml
+    /// </summary>
+    public partial class NewProductPurchaseContentDialog : ContentDialog
+    {
+        private readonly Action<ProductPurchase>? _saveProductPurchase;
+        private readonly ContentPresenter? _contentPresenter;
+
+        public NewProductPurchaseContentDialog(ContentPresenter? contentPresenter, Action<ProductPurchase> saveProductPurchase) : base(contentPresenter)
+        {
+            _contentPresenter = contentPresenter;
+            _saveProductPurchase = saveProductPurchase;
+            InitializeComponent();
+        }
+    }
+}
