ae723e55850566203b752d769983af4a39cf6ecd
ddani-commits <23090584@alumno.utmetropolitana.edu.mx>
2025-06-18 13:48:24 -0600

    fix model

--- AsComputoPOS/Models/PurchaseOrder.cs
+++ AsComputoPOS/Models/PurchaseOrder.cs
@@ -21,8 +21,13 @@
     // Product purchase represents each one of the items that are being purchased in a purchase order
     public class ProductPurchase {
         public int Id { get; set; }
+
         public int ProductId { get; set; }
         public Product Product {get; set;} // references the product being resupplied
+        
+        public int PurchaseOrderId { get; set; }
+        public PurchaseOrder PurchaseOrder { get; set; }
+        
         public decimal UnitPrice {get; set;} // represents the purchase price from the business perspective
         public decimal Quantity {get; set;} // represents how many units were bougth in this batch
         public decimal Subtotal {get; set;} // represents the price before discounts, etc.
