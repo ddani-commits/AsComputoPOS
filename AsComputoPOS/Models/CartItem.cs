271d317c694d90c3db34001977eb7bf550ca2bcc
ddani-commits <23090584@alumno.utmetropolitana.edu.mx>
2025-07-01 13:18:24 -0600

    add models

---
+++ AsComputoPOS/Models/CartItem.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+
+namespace TamoPOS.Models
+{
+    public class CartItem : INotifyPropertyChanged
+    {
+        private int _productId;
+        private Product _product;
+        private decimal _unitPrice;
+        private decimal _quantity;
+
+        public int ProductId
+        {
+            get => _productId;
+            set
+            {
+                if (_productId != value)
+                {
+                    _productId = value;
+                    OnPropertyChanged(nameof(ProductId));
+                }
+            }
+        }
+
+        public Product Product
+        {
+            get => _product;
+            set
+            {
+                if (_product != value)
+                {
+                    _product = value;
+                    OnPropertyChanged(nameof(Product));
+                }
+            }
+        }
+
+        public decimal UnitPrice
+        {
+            get => _unitPrice;
+            set
+            {
+                if (_unitPrice != value)
+                {
+                    _unitPrice = value;
+                    OnPropertyChanged(nameof(UnitPrice));
+                    OnPropertyChanged(nameof(Total));
+                }
+            }
+        }
+
+        public decimal Quantity
+        {
+            get => _quantity;
+            set
+            {
+                if (_quantity != value)
+                {
+                    _quantity = value;
+                    OnPropertyChanged(nameof(Quantity));
+                    OnPropertyChanged(nameof(Total));
+                }
+            }
+        }
+
+        public decimal Total => Quantity * UnitPrice;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- AsComputoPOS/Models/PurchaseOrder.cs
+++ AsComputoPOS/Models/PurchaseOrder.cs
@@ -1,54 +1,20 @@
 ﻿namespace TamoPOS.Models
 {
     // Represents the general term of purchase order, which is a request to buy products from a supplier
-    public class PurchaseOrder 
+    public class PurchaseOrder
     {
         public int Id { get; set; }
         public int SupplierId { get; set; }
         public Supplier Supplier { get; set; }
         public DateTime PurchaseDate { get; set; }
-        public decimal? Total {get; set;}
-        public decimal? Subtotal {get; set;}
+        public decimal? Total { get; set; }
+        public decimal? Subtotal { get; set; }
         public ICollection<ProductPurchase>? ProductPurchases { get; set; }
         public PurchaseOrder() { }
-        public PurchaseOrder(Supplier supplier, DateTime purchaseDate) 
+        public PurchaseOrder(Supplier supplier, DateTime purchaseDate)
         {
             Supplier = supplier;
             PurchaseDate = purchaseDate;
         }
-    }
-
-    // Product purchase represents each one of the items that are being purchased in a purchase order
-    public class ProductPurchase {
-        public int Id { get; set; }
-
-        public int ProductId { get; set; }
-        public Product Product {get; set;} // references the product being resupplied
-        
-        public int PurchaseOrderId { get; set; }
-        public PurchaseOrder PurchaseOrder { get; set; }
-        
-        public decimal UnitPrice {get; set;} // represents the purchase price from the business perspective
-        public decimal Quantity {get; set;} // represents how many units were bougth in this batch
-        public decimal Subtotal {get; set;} // represents the price before discounts, etc.
-        public decimal Total {get; set;} // represents the price after discounts, etc.
-        public decimal? FlatProfitMargin { get; set; }    // e.g. add $5 to the unit price
-        public decimal? PercentProfitMargin { get; set; } // e.g. add 10% to the unit price
-        public decimal SalePrice {get; set;} // represents the price of the product after adding the markup/profit marginS
-        public decimal? QuantityRemaining { get; set; }
-        //public ICollection<StockBatch> StockBatches { get; set; } // there might be multiple batches for the same product purchase, each with its own stock left
-    }
-
-    // what in spanish would be called "lote"
-    // allows to track stock of products within the same price, so the user can restock before the stock runs out,
-    // and still keep the old pricing
-    // feeling like this is overkill
-    public class StockBatch {
-        public int Id { get; set; }
-        public Product Product { get; set; }
-        public ProductPurchase ProductPurchase { get; set; }
-        public decimal QuantityPurchased { get; set; }
-        public decimal QuantityRemaining { get; set; }
-        public DateTime ReceivedDate { get; set; }
     }
 }
