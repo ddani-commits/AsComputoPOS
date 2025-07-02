dcdf51d7ea4194a8e496608f76cc3736b433ca76
ddani-commits <23090584@alumno.utmetropolitana.edu.mx>
2025-07-02 11:33:50 -0600

    update models

--- AsComputoPOS/Data/ApplicationDbContext.cs
+++ AsComputoPOS/Data/ApplicationDbContext.cs
@@ -11,6 +11,9 @@ namespace TamoPOS.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<PurchaseOrder> PurchaseOrders { get; set; }
         public DbSet<ProductPurchase> ProductPurchases { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=app.db"); 
