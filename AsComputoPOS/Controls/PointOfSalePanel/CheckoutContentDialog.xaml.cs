5cf3d89550e7e68b258dee233ab10d5c38c38594
ddani-commits <23090584@alumno.utmetropolitana.edu.mx>
2025-07-03 13:43:39 -0600

    dialog

---
+++ AsComputoPOS/Controls/PointOfSalePanel/CheckoutContentDialog.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+using TamoPOS.Services;
+using Wpf.Ui.Controls;
+
+namespace TamoPOS.Controls.PointOfSalePanel
+{
+    public partial class CheckoutContentDialog : ContentDialog
+    {
+        private readonly IPOSService _posPanelService;
+        private ContentPresenter? _contentPresenter;
+
+        public CheckoutContentDialog
+        (
+            ContentPresenter? contentPresenter, 
+            IPOSService posPanelService
+        ) : base(contentPresenter)
+        {
+            InitializeComponent();
+            DataContext = this;
+            _posPanelService = posPanelService;
+            _contentPresenter = contentPresenter;
+        }
+    }
+}
